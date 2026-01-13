17651a21e81cf2d8a492bec2cfc6c94fe43bf617
Aakash Thapliyal <iaakashthapliyal@gmail.com>
2026-01-13 22:45:22 +0530

    feat: complete GitOps project structure

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,7 @@
-@"
 FROM node:18-slim
 WORKDIR /app
 COPY package*.json ./
-RUN npm install
+RUN npm install --only=production
 COPY . .
 EXPOSE 3000
 CMD ["node", "src/app.js"]
-"@ | Out-File -FilePath Dockerfile -Encoding utf8
--- src/app.js
+++ src/app.js
@@ -1,11 +1,11 @@
-@ "
 const express = require('express');
 const app = express();
 const port = 3000;
+
 app.get('/', (req, res) => {
-    res.send('<h1>GitOps Success!</h1><p>Running on: AakashThapliyal/exp1_k8s</p>');
+    res.send('<h1>GitOps Experiment Successful!</h1><p>Managed by Argo CD | Version: 1.0.0</p>');
 });
+
 app.listen(port, () => {
-    console.log('App running on port 3000');
+    console.log(`App listening at http://localhost:${port}`);
 });
-"@ | Out-File -FilePath src\app.js -Encoding utf8
